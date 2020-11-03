797bc1750819bf15dde70cd85efbf374b7937dae
Александр Зимогоров <zimniimars@gmail.com>
2020-11-04 01:27:56 +0300

    test

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 4;        
+            ushort b = a;
+            ushort a1 = a;
+            sbyte c = 3;
+            short d = c;
+            short c1 = c;
+            sbyte e = -4;
+            short f = e;
+            short e1 = c;
+            byte r = (byte)(c + e);
+            int k = (int)(a + b - e);
+            short l = (short)(c + e);
+        }
+    }
+}
