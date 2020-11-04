423d16132924e3c16d9f56db09b53ee7b9e07d3d
Александр Зимогоров <zimniimars@gmail.com>
2020-11-04 12:41:54 +0300

    Task2 done

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 13;
+            System.Int32 a2 = 14;
+            sbyte a3 = 15;
+            System.SByte a4 = 16;
+
+            Console.WriteLine("a1: " + a1.GetType());
+            Console.WriteLine("a2: " + a2.GetType());
+            Console.WriteLine("a3: " + a3.GetType());
+            Console.WriteLine("a4: " + a4.GetType());
+        }
+    }
+}
